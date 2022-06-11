fa50dead7bfc9c2560cfc2ec38bbca1698fa17b8
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-06-11 10:40:59 -0300

    Actualizacion

---
+++ Clases/Clase2-Condicionales/complementario.js
@@ -0,0 +1,47 @@
+/*
+//--- Estructura del if ---//
+
+// si - condicion
+if (true) {
+	// bloque de código a ejecutar
+    console.log("vas a ver este mensaje");
+}
+
+//--- Comparacion ---//
+
+Cuando utilizamos operadores matemáticos entre 
+dos valores numéricos, éstos resuelven un nuevo 
+tipo de valor numérico que es el resultado de 
+la operación. Cuando comparamos dos valores a 
+través de un operador de comparación, ésta operación 
+siempre se resuelve en true o false, es decir 
+la comparación es verdadera o falsa.
+*/
+
+//--- If Else ---//
+/*
+En ocasiones, las decisiones que se deben realizar 
+no son del tipo «si se cumple la condición, hazlo; 
+si no se cumple, no hagas nada». Normalmente las 
+condiciones suelen ser del tipo «si se cumple 
+esta condición, hazlo; si no se cumple, haz esto 
+otro».
+
+let unColor = "Rojo"
+
+// Con (unColor == "Rojo") comparamos si unColor es igual "Rojo"
+if (unColor == "Rojo"){
+    console.log("el color es Rojo");
+}else{
+//La instrucción se interpreta cuando unColor NO es "Rojo"
+    console.log("el color NO es Rojo");
+}
+*/
+//--- Algoritmo numero par ---//
+// const numero = 200;
+
+// if(numero % 2 ===0){
+//     console.log("El numero es par")
+// } else {
+//     console.log("El numero es impar")
+// }
