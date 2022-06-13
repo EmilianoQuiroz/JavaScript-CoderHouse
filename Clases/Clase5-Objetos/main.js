a0aa87ec08d070067901afecd4fb033018f8fc57
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-06-13 19:10:19 -0300

    Actualizacion

--- Clases/Clase5-Objetos/main.js
+++ Clases/Clase5-Objetos/main.js
@@ -103,3 +103,135 @@ function agregarAlCarrito(producto){
 }
 //011852
 
+/*--- MÉTODO <> FUNCIÓN ---*/
+/*
+Como vimos anteriormente, las funciones en JS se pueden definir en cualquier parte del código, y pueden ser llamadas desde cualquier otra parte del código posterior.
+Los métodos de los objetos también son técnicamente funciones, sólo que se limitan a poder ser ejecutados solo desde el mismo objeto.
+*/
+
+/*--- FUNCIÓN ---*/
+/*
+//Funciones: Generalmente retornar un valor y 
+
+son de acceso global.
+function f1(){
+    return this;
+}
+*/
+
+/*--- MÉTODO ---*/
+/*
+//Métodos: Se requiere un objeto y puede no 
+
+retornar un valor.
+function Persona(nombre, edad, calle) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.calle = calle;
+}
+*/
+
+/*--- MÉTODOS EN OBJETOS JS ---*/
+/*
+JavaScript cuenta con sus propios objetos, incluso ya usamos algunos de ellos sin identificar que son objeto.
+Por ejemplo: Cada vez que creamos una cadena de caracteres se crea automáticamente como una instancia del objeto String y, por lo tanto, tiene varios métodos/propiedades comunes disponibles en ella.
+
+let cadena = "HOLA CODER";
+//Propiedad de objeto String: Largo de la cadena.
+console.log(cadena.length);
+//Método de objeto String: Pasar a minúscula.
+console.log(cadena.toLowerCase());
+//Método de objeto String: Pasar a mayúscula.
+console.log(cadena.toUpperCase());
+*/
+
+/*--- MÉTODOS PERSONALIZADOS ---*/
+/*
+Podemos crear nuestro propios métodos para objetos personalizados, referenciando funciones por su nombre o definiendo funciones anónimas asociadas a una propiedad de la función constructora. 
+Llamar a un método es similar a acceder a una propiedad, pero se agrega () al final del nombre del método, posiblemente con argumentos.
+
+function Persona(nombre, edad, calle) {
+    this.nombre = nombre;
+    this.edad   = edad;
+    this.calle  = calle;
+    this.hablar = function(){ console.log("HOLA SOY "+ this.nombre)}
+}
+const persona1 = new Persona("Homero", 39, "Av. Siempreviva 742");
+const persona2 = new Persona("Marge", 36, "Av. Siempreviva 742");
+persona1.hablar();
+persona2.hablar();
+*/
+
+/*--- OPERADOR IN Y FOR...IN ---*/
+/*
+El operador in devuelve true si la propiedad especificada existe en el objeto. 
+Mientras que el bucle for...in permite acceder a todas las propiedades del objeto, obteniendo una propiedad por cada iteración.
+
+const persona1 = { nombre: "Homero", edad: 39, calle: "Av. Siempreviva 742"};
+//devuelve true porque la clave "nombre" existe en el objeto persona1
+console.log( "nombre" in persona1);
+//devuelve false porque la clave "origen" no existe en el objeto persona1
+console.log( "origen" in persona1);
+//recorremos todas las propiedades del objeto con el ciclo for...in
+for (const propiedad in persona1) {
+    console.log(persona1[propiedad]);
+}
+
+*/
+
+/*--- CLASES ---*/
+/*
+Las clases de JavaScript, introducidas en ES6, proveen una sintaxis mucho más clara y simple para crear objetos personalizados.
+Son una equivalencia al empleo de función constructora y permite definir distintos tipo de métodos.
+
+class Persona{
+    constructor(nombre, edad, calle) {
+        this.nombre = nombre;
+        this.edad   = edad;
+        this.calle  = calle;
+    }
+}
+const persona1 = new Persona("Homero", 39, "Av. Siempreviva 742");
+*/
+
+/*--- CLASES Y MÉTODOS ---*/
+/*
+En la declaración de clase, la función constructora es reemplazada por el método constructor. Los métodos en las clases no referencian a propiedades, se declaran dentro del bloque sin la palabra function.
+
+class Persona{
+    constructor(nombre, edad, calle) {
+        this.nombre = nombre;
+        this.edad   = edad;
+        this.calle  = calle;
+    }
+    hablar(){
+        console.log("HOLA SOY "+ this.nombre);
+    }
+}
+const persona1 = new Persona("Homero", 39, "Av. Siempreviva 742");
+persona1.hablar();
+*/
+
+/*
+EJEMPLO
+
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre  = nombre.toUpperCase();
+        this.precio  = parseFloat(precio);
+        this.vendido = false;
+    }
+    sumaIva() {
+        this.precio = this.precio * 1.21;
+    }
+    vender() {
+        this.vendido = true;
+    }
+}
+const producto1 = new Producto("arroz", "125");
+const producto2 = new Producto("fideo", "50");
+producto1.sumaIva();
+producto2.sumaIva();
+producto1.vender();
+
+*/
