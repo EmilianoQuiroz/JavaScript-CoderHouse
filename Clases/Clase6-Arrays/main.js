1e6884697c5fb16e26b658beb33e901174c8fb95
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-06-14 09:02:12 -0300

    Actualizacion

--- Clases/Clase6-Arrays/main.js
+++ Clases/Clase6-Arrays/main.js
@@ -101,34 +101,190 @@ console.log(miArray)
 
 */
 //Ejemplo Carrito de Compras
-const productosEnELCarrito = [];
+// const productosEnELCarrito = [];
 
-const productos = [{ id: 01,
-                     titulo: 'Zapatillas',
-                     precio: '900'},
-                    {id: 02, 
-                     titulo: 'Remeras',
-                     precio: 200},
-                    {id: 03, 
-                     titulo: 'Buzos',
-                     precio: 1200}];
+// const productos = [{ id: 01,
+//                      titulo: 'Zapatillas',
+//                      precio: '900'},
+//                     {id: 02, 
+//                      titulo: 'Remeras',
+//                      precio: 200},
+//                     {id: 03, 
+//                      titulo: 'Buzos',
+//                      precio: 1200}];
 
-let acumulador = '';
+// let acumulador = '';
 
-for(let i= 0; i < productos.length; i++) {
+// for(let i= 0; i < productos.length; i++) {
     
-    acumulador += `<div> 
-    $${productos[i].titulo} - $${productos[i].precio}<br>
-    <button onclick="agregarAlCarrito(${productos[i].id})">Agregar<button></div>`;
+//     acumulador += `<div> 
+//     $${productos[i].titulo} - $${productos[i].precio}<br>
+//     <button onclick="agregarAlCarrito(${productos[i].id})">Agregar<button></div>`;
     
+// }
+
+// document.write(acumulador);
+
+// function agregarAlCarrito(idDeProducto){
+//     console.log(idDeProducto);
+//     productosEnELCarrito.push(idDeProducto);
+//     console.log(`tenes ${productosEnELCarrito.
+//length} producto/s en el carrito`);
+//     console.log(productosEnELCarrito)
+//}
+/*--- QUITAR ELEMENTOS ---*/
+/*
+De forma inversa, existen métodos para eliminar elementos del array.
+Si queremos eliminar el primer elemento del array utilizamos el método shift(); y si queremos eliminar el último elemento, el método pop().
+*/
+
+/*--- POP y SHIFT ---*/
+/*
+const nombres = ["Luis", "Ana", "Julia", “Juan”]
+
+nombres.pop()
+console.log(nombres) // ["Luis", "Ana", "Julia"]
+
+nombres.shift()
+console.log(nombres) // ["Ana", "Julia"]
+*/
+
+/*--- REVERSE ---*/
+/*
+Sirve para dar vuelta el array y se escribe de la siguiente manera:
+
+nombreDelArray.reverse()
+*/
+
+/*--- SPLICE ---*/
+/*
+El método splice() permite eliminar uno o varios elementos de un array en cualquier posición. Funciona con 2 parámetros: el primero es el índice donde se ubica el método para trabajar, y el segundo es la cantidad de elementos a eliminar desde esa posición.
+
+const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+nombres.splice(1, 2)
+
+console.log(nombres)
+// ['Rita', 'Ana', 'Vanesa']
+*/
+
+/*--- JOIN ---*/
+/*
+Mediante el método join  podemos generar un string con todos los elementos del array, separados por el valor que pasamos por parámetro:
+
+const nombres = ["Luis", "Ana", "Julia", “Juan”]
+
+console.log( nombres.join(“, ”) ) 
+// Luis, Ana, Julia
+
+console.log( nombres.join(“*”) ) 
+// Luis*Ana*Julia
+
+*/
+
+/*--- CONCAT ---*/
+/*
+Mediante el método concat podemos combinar dos Arrays en un único Array resultante:
+
+const perros   = ["Pupy", “Ronnie”]
+const gatos = ["Mishi", “Garfield”, "Zuri"]
+const mascotas = perros.concat(gatos)
+console.log(mascotas)
+// ["Pupy", “Ronnie”, "Mishi", “Garfield”, "Zuri"]
+*/
+
+/*--- SLICE ---*/
+/*
+El método slice devuelve una copia de una parte del Array dentro de un nuevo Array, empezando por el inicio hasta fin (fin no incluído). El Array original no se modificará.
+
+const nombres    = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+const masculinos = nombres.slice(1, 3); // Nuevo array desde la posición 1 a 3.
+// masculinos contiene ['Pedro','Miguel']
+*/
+
+/*--- INDEXOF ---*/
+/*
+El método indexOf() nos permite obtener el índice de un elemento en un array. Recibe por parámetro el elemento que queremos buscar en el array y, en caso de existir, nos retorna su índice. Si el elemento no existe nos retornará como valor: -1
+
+const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+
+console.log( nombres.indexOf('Rita') ) // ⇒ 0
+console.log( nombres.indexOf('Ana') ) // ⇒ 3
+console.log( nombres.indexOf('Julieta') ) // ⇒ -1
+*/
+
+/*--- INCLUDES ---*/
+/*Similar al anterior, el método includes me permite saber si un elemento que recibo por parámetro existe o no dentro de un array, retornando un valor booleano en caso afirmativo o negativo:
+
+const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa']
+
+console.log( nombres.includes('Rita') ) // ⇒ true
+console.log( nombres.includes('Miguel') ) // ⇒ true
+console.log( nombres.includes('Julieta') ) // ⇒ false
+*/
+
+/*--- REVERSE ---*/
+/*
+Como su nombre lo indica, el método reverse() invierte el orden de los elementos dentro de un array.
+
+const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa']
+nombres.reverse()
+console.log( nombres ) 
+// ⇒ ['Vanesa', 'Ana','Miguel', 'Pedro','Rita']
+*/
+
+/*--- EJEMPLO APLICADO: CARGAR ARRAY CON ENTRADAS ---*/
+
+//Declaraciòn de array vacío y variable para determinar cantidad
+// const listaNombres = [];
+// let   cantidad     = 5;
+//Empleo de do...while para cargar nombres en el array por prompt()
+// do{
+//    let entrada = prompt("Ingresar nombre");
+//    listaNombres.push(entrada.toUpperCase());
+//    console.log(listaNombres.length);
+// }while(listaNombres.length != cantidad)
+// //Concatenamos un nuevo array de dos elementos
+// const nuevaLista = listaNombres.concat(["ANA","EMA"]);
+//Salida con salto de línea usando join
+// alert(nuevaLista.join("\n"));
+
+/*--- ARRAY DE OBJETOS ---*/
+/*
+Los array pueden usarse para almacenar objetos personalizados. Podemos asignar objetos literales o previamente instanciados en la declaración del array o agregar nuevos objetos usando el método push y el constructor.
+
+const objeto1 = { id: 1, producto: "Arroz" };
+const array   = [objeto1, { id: 2, producto: "Fideo" }];
+array.push({ id: 3, producto: "Pan" });
+*/
+
+/*--- ¡ARRAYS + OBJETOS! ---*/
+/*
+La combinación de arrays con objetos genera estructuras complejas de datos. 
+¡Los métodos de arrays y las herramientas para recorrerlos nos permiten acceder y manipular todos estos datos de forma precisa y prolija!
+*/
+
+/*--- FOR...OF ---*/
+/*
+La sentencia for...of permite recorrer un array ejecutando un bloque de código por cada elemento del objeto.
+
+const productos = [{ id: 1, producto: "Arroz" },
+                  { id: 2,  producto: "Fideo" },
+                  { id: 3,  producto: "Pan" }];
+
+for (const producto of productos) {
+    console.log(producto.id);
+    console.log(producto.producto);
 }
 
-document.write(acumulador);
+Es un iterador que recorre el array de principio a fin, y en cada iteración accedemos al elemento en cuestión a través de la referencia que declaramos. Por cada iteración se ejecuta el bloque de código que definimos entre llaves.
 
-function agregarAlCarrito(idDeProducto){
-    console.log(idDeProducto);
-    productosEnELCarrito.push(idDeProducto);
-    console.log(`tenes ${productosEnELCarrito.length} producto/s en el carrito`);
-    console.log(productosEnELCarrito)
-}
-/*---  ---*/
+for (const referencia of array) { … }
+
+*/
+
+
+
+
+
+
+
